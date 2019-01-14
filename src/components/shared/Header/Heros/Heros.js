075fc11d8660f9188ff19df6412208a128256c26
Dominique Coupal <info@eden6.com>
2019-01-14 13:55:13 -0500

    Remplacement de la photo heros pour img-responsive

--- src/components/shared/Header/Heros/Heros.js
+++ src/components/shared/Header/Heros/Heros.js
@@ -5,6 +5,7 @@ import classes from './Heros.module.scss';
 const Heros = (props) => {
     return (
         <section className={classes.Heros}>
+            <img srcSet="/img/heros_small.jpg 640w, /img/heros_medium.jpg 1280w, /img/heros_full.jpg 2048w" src="/img/heros_medium.jpg" alt="Code javascript" className={classes.HerosBg}/>
             <h1 className={classes.HerosTitle}>{props.title}</h1>
         </section>
     );
