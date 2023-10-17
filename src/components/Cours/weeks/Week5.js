33c3e089886fde847eae79187343ca679261bfb0
dcoupalpprof <dcoupalpprof@users.noreply.github.com>
2023-10-16 21:18:13 -0400

    Update Week5.js

--- src/components/Cours/weeks/Week5.js
+++ src/components/Cours/weeks/Week5.js
@@ -413,7 +413,7 @@ export default defineConfig({
 // Fichier cypress.config.js
 // Dans l'objet e2e, ajouter les propriétés suivantes:
     baseUrl: 'http://127.0.0.1:5173',
-    specPattern: 'cypress/e2e/**/*.{js.jsx.ts.tsx}'
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}'
                     `}/>
                 </Groupe>
                                 <Groupe title="Création d'un premier test">
